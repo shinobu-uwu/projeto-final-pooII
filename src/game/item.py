f93cab1bca4ad2767cc085fdae2471444dc672e5
Matheus <matheus.reinert@protonmail.com>
2020-11-18 18:27:03 -0300

    Arrumar imports

--- src/game/item.py
+++ src/game/item.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from src.game.interfaces.interface_item import IItem
+from interfaces.interface_item import IItem
 
 
 class Item(IItem):
--- src/game/jogador.py
+++ src/game/jogador.py
@@ -1,5 +1,5 @@
-from src.game.inventario import Inventario
-from src.game.interfaces.interface_jogador import IJogador
+from inventario import Inventario
+from interfaces.interface_jogador import IJogador
 
 
 class Jogador(IJogador):

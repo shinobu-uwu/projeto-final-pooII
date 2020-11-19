3be5b7138e760b0f77349895fc2ca2796c76e67b
Matheus <matheus.reinert@protonmail.com>
2020-11-19 18:36:05 -0300

    Acabar jogador

--- src/game/jogador.py
+++ src/game/jogador.py
@@ -9,3 +9,39 @@ class Jogador(IJogador):
         self.__velocidade = 5#temporário
         self.__item_equipado = 0
         self.__posicao = [0, 10]#temporário
+
+    def mover(self, tecla):
+        pass
+
+    def pular(self):
+        pass
+
+    def usar(self):
+        pass
+
+    def mudar_item(self, tecla):
+        pass
+
+    @property
+    def morto(self):
+        return self.__morto
+
+    @property
+    def inventario(self):
+        return self.__inventario
+
+    @property
+    def velocidade(self):
+        return self.__velocidade
+
+    @property
+    def item_equipado(self):
+        return self.__item_equipado
+
+    @property
+    def posicao(self):
+        return self.__posicao
+
+    @morto.setter
+    def morto(self, morto):
+        self.__morto = morto

cce51c8f510efc60e92be26f26e8c19de5cd5eca
Matheus Filipe dos Santos Reinert <62085690+shinobu-uwu@users.noreply.github.com>
2020-11-22 20:53:44 -0300

    Mover export para antes dos imports

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+#Para os imports funcionarem corretamente é necessário exportar a env variable PYTHONPATH
+os.environ["PYTHONPATH"] = os.path.abspath(os.path.dirname(__file__))
+
 from src.controller.controller import Controller
 import os
 
-#Para os imports funcionarem corretamente é necessário exportar a env variable PYTHONPATH
-os.environ["PYTHONPATH"] = os.path.abspath(os.path.dirname(__file__))
 a = Controller()
 a.comeca()

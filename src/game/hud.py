c152ea029983c6a3a59d67533aa8f87460e1d6e6
BrunoNoGithub <brunopamplona10@hotmail.com>
2020-11-28 19:05:53 -0300

    adição de assets para uso na hud e seleção de itens no inventario

--- src/game/hud.py
+++ src/game/hud.py
@@ -6,16 +6,17 @@ from pygame.locals import *
 class HUD(IHud):
     def __init__(self,jogador: Jogador):
         self.__jogador=jogador
-
+        #adcionar imagens de slot de inventario
         self.space=pygame.Surface((30,30))
-        #self.space=self.space.fill((0,0,0))
+        
 
     def atualizar(self,screen,time):
-        for i in range (0,6):
-            screen.blit(self.space,(20+i*35,10))
+        greenimage=pygame.Surface((30,10))
+        greenimage.fill((255,0,255))
+        screen.blit(greenimage,(20+(self.__jogador.item_equipado)*35,55))
+        for i in range(0,6):
+            screen.blit(self.space,(20+i*35,20))
 
         text_surface=pygame.font.Font(pygame.font.match_font('arial'),20).render("Tempo"+str(time)+"segs",True,(255,255,255))
         #text_surface=pygame.font.Font(font_name,size).render("Time:"+str(time),True,(255,255,255))
-        #text_rect=text_surface.get_rect()
-        #text_rect.midtop=(x,y)
         screen.blit(text_surface,(1150,20))

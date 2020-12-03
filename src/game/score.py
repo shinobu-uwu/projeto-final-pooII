82f349a83583d9178cd9e11e9c407deaf3f63110
Matheus <matheus.reinert@protonmail.com>
2020-12-03 00:10:14 -0300

    Adicionar data nos scores

--- src/game/score.py
+++ src/game/score.py
@@ -1,12 +1,15 @@
+import datetime
+
 class Score:
     def __init__(self, tempo, fase, jogador):
         self.__tempo = tempo
         self.__fase = fase
         self.__jogador = jogador
+        self.__data = datetime.datetime.now()
 
 
     def to_dict(self):
-        score = {"tempo": str(self.__tempo), "fase": str(self.__fase)}
+        score = {"tempo": str(self.__tempo), "fase": str(self.__fase), "data": str(self.__data)}
         return score
 
     @property
